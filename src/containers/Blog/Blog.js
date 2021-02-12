bba5e2d92d83230e59a7e27b784004512d5ada7d
James Atkinson <CSAtkinson8588@gmail.com>
2021-02-12 11:59:32 -0500

    .

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+
 import './Blog.module.css';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
+import asyncComponent from '../../hoc/asyncComponent'
+//import NewPost from './NewPost/NewPost'
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 
 
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render() {
@@ -31,7 +37,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
 
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null} 
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null} 
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                     <Route path="/" component={Posts} />
